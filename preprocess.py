294fd14b245bfc4c14eac3eb98e66834b09357ca
Implementist <yuyuyu4993@qq.com>
2018-09-08 17:57:01 +0800

    Initial commit
    Such as the title.

---
+++ preprocess.py
@@ -0,0 +1,99 @@
+# coding:utf8
+
+'''
+
+输入一个仓库(文件夹路径)
+
+1. 提取其中的html，js，css文件
+
+2. 变量替换 href，src等引用部分 以及 script片段 (用任意变量名替换)
+
+'''
+
+import shutil
+import sys
+import os
+import re
+
+
+# 输入仓库路径，返回所有文件的绝对路径（二维list），list[0]是文件名，list[1]是文件路径
+def get_hcj_list(repo_path):
+    path_list = []
+
+    pat = '(\.html|\.css|\.js)$'  # 提取html,css,js的正则
+
+    if os.path.isdir(repo_path):  # 判断repo_path是否是一个文件夹
+        for root, dirs, files in os.walk(repo_path):
+            for file in files:
+                r = re.compile(pat).findall(file)
+                if len(r) != 0:
+                    abs_file_path = os.path.join(root, file)
+                    path_list.append([file, abs_file_path])
+    else:
+        print('[!] dir path error. ')
+    return path_list
+
+
+# 输入文件路径的二维list，将它们移至目标文件夹target_dir
+def shift_file(file_list, target_dir):
+    if os.path.isdir(target_dir):
+        for file_name, file_path in file_list:
+            shutil.copyfile(file_path, target_dir + '/' + file_name)
+
+
+# 输入文件路径的二维list，将其中所有文件的引用部分和script片段替换为任意变量
+def replace_quote(file_list):
+    html_pat = '\.html$'  # 正则判断是否为html文件
+
+    all_pat = '=".*?\.!(js|png|jpg|gif)"|=\'.*?\.!(js|png|jpg|gif)\''
+    js_pat = '=".*?\.js"|=\'.*?\.js\'|=.*?\.js '  # 正则替换js
+    pic_pat = '=".*?\.(png|jpg|gif)"|=\'.*?\.(png|jpg|gif)\'|=.*?\.(png|jpg|gif) '  # 正则替换图片
+    script_pat = '<script>.*?</script>'  # 正则替换script脚本
+
+    for f_name, f_path in file_list:
+
+        r = re.compile(html_pat).findall(f_path)
+        if len(r) != 0:  # 是.html文件
+            with open(f_path, 'r') as html_file:
+                text = re.compile(js_pat, re.S).sub('="a.js"', html_file.read())
+                text = re.compile(pic_pat, re.S).sub('="b.png"', text)
+                text = re.compile(script_pat, re.S).sub('<script>//script fragment</script>', text)  # 替换script片段为空
+                text = re.compile(all_pat, re.S).sub('="abc"', text)
+
+            with open(f_path, 'w') as html_file:
+                html_file.write(text)
+
+
+def xhelp():
+    print("[*] -d, --repo_dir       要提取html,css,js的目录路径    <> 如 './ddd_dir'")
+    print("[*] -o, --output_dir     结果输出文件夹                 <> 默认'./result'")
+    print("[*] -h, --help           help帮助                       <> print this help")
+    print("[*] Example : python hcj_sep.py -d '320andup-master'")
+    sys.exit(1)
+
+
+if __name__ == '__main__':
+    repo_path = './datasets/pure-master'
+    output_dir = './result'  # 输出目录
+
+    try:
+        for argv in sys.argv:
+            if argv.lower() == "-d" or argv.lower() == "--repo_dir":
+                repo_path = sys.argv[sys.argv.index(argv) + 1]
+            elif argv.lower() == "-o" or argv.lower() == "--output_dir":
+                output_dir = sys.argv[sys.argv.index(argv) + 1]
+            elif argv.lower() == "-h" or argv.lower() == "--help":
+                xhelp()
+    except SystemExit:
+        print("[!] Cheak your parametars input")
+        sys.exit(0)
+    except Exception:
+        xhelp()
+
+    if os.path.exists(output_dir) == False:  # 若文件不存在则新建文件夹
+        os.makedirs(output_dir)
+
+    file_list = get_hcj_list(repo_path)  # 得到目标文件夹的htmljscss文件路径
+    shift_file(file_list, output_dir)  # 复制文件到新目录下
+    new_file_list = get_hcj_list(output_dir)  # 获取新目录下的文件路径
+    replace_quote(new_file_list)  # 变量替换新文件
