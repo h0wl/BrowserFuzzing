5b40c8729aabf60fc28a0d795c4028bb1b0501a0
77Sera <sakainozomi77@gmail.com>
2018-09-25 21:40:55 +0800

    Update 对不在字典中的token进行分类返回一个dict

--- html2vectorMatrix.py
+++ html2vectorMatrix.py
@@ -1,8 +1,7 @@
 #coding:utf8
 
 '''
-html转向量的函数集合
-core target=>输入 html文件 输出一个向量矩阵
+输入 html文件 输出一个向量矩阵
 忽略:
 	- 标签间的内容
 	- 不规范的标签书写方式
@@ -11,6 +10,7 @@ core target=>输入 html文件 输出一个向量矩阵
 import random
 import sys
 import re
+import os
 
 # 从本地文件读取标签并返回list(这两个键值对相反)
 def getTagList():
@@ -86,11 +86,9 @@ def string2matrix(str,tagList):
 def deepSplit(str):
 	result = []
 	
-	pat1 = "^(/)(.*)" # 匹配类似 /body   /p
-	
-	pat2 = '''(.*?)(=)(["'])(.*?)(["'])'''
-	
-	# -----------------------------
+	pat1 = "^(/)(.*)" # 匹配类似 => /body
+	pat2 = '''(.*?)(=)(["'])(.*?)(["']{0,1})''' # 匹配类似 => class="test 
+	pat3 = "(.*?)(\")" # 匹配类似 => test"
 	
 	data_pat = "data(-.*)" # 匹配 data-*
 	
@@ -99,7 +97,9 @@ def deepSplit(str):
 	if len(r) == 0:
 		r = re.compile(pat2).findall(str)
 		if len(r) == 0:
-			result.append(str)
+			r = re.compile(pat3).findall(str)
+			if len(r) == 0:
+				result.append(str)
 			
 	for tuple in r:
 		for i in tuple:
@@ -122,67 +122,51 @@ def matrix2string(matrix,tagList):
 		string+=' '
 	return string
 	
-# 分析当前向量矩阵，查看未转换为向量的词有哪些,accuracy表示上下文精度
-def analyzeMatrix(matrix,tagList,accuracy=4):
-	for i in range(len(matrix)): # matrix[i]代表一个向量
+# 分析当前向量矩阵，查看未转换为向量的词有哪些
+def analyzeMatrix(matrix,tagList,accuracy=4,doPrint=True):
+	token_list = []
+	for i in range(len(matrix)):
 		if type(matrix[i]).__name__ == 'str': # 表示未转成向量
-			string = ''
-			for j in range(accuracy):
-				try: string+=tagList[matrix[i-accuracy+j].index(1)]+' '
-				except: pass
-			string+=matrix[i]
-			for j in range(accuracy):
-				try: string+=tagList[matrix[i+j+1].index(1)]+' '
-				except: pass
-			print('[*] 陌生TAG=>'+matrix[i]+'\t上下文=>'+string)
-
-if __name__ == '__main__':
-	tagList = getTagList() # 读取本地字典
+			token_list.append(matrix[i])
+			if doPrint: # 要打印分析
+				string = ''
+				for j in range(accuracy):
+					try: string+=tagList[matrix[i-3+j].index(1)]+' '
+					except: pass
+				string+=matrix[i]
+				for j in range(accuracy):
+					try: string+=tagList[matrix[i+j+1].index(1)]+' '
+					except: pass
+				print('[*] 陌生TAG=>'+matrix[i]+'\t上下文=>'+string)
+	return token_list
 	
-	html_str = readFile('./index.html') # 读取本地index.html文件
+# 输入一个html文件夹，检索其中的10个html文件并返回其中的“陌生”token
+def checkHTML(target_path,tagList,checkNum=10,doPrint=False):
+	count = 0
+	lost_token = []
+	for i in os.listdir(target_path):
+		html_path = target_path+'/'+i
+		if os.path.isfile(html_path):
+			html_str = readFile(html_path) # 读取文件获取字符串
+			vector_matrix = generateMatrix(html_str,tagList) # 获取向量矩阵
+			temp_token_list = analyzeMatrix(vector_matrix,tagList,doPrint=False)
+			for token in temp_token_list:
+				if token != '':
+					lost_token.append(token)
+					if doPrint:
+						print(token)
+			if checkNum != False:
+				count+=1
+				if count == checkNum:
+					break
+	return lost_token
 	
-	# 获取向量矩阵 形式:2维list; 矩阵维数: token数*字典数
-	vector_matrix = generateMatrix(html_str,tagList) 
-	
-	# 打印向量矩阵
-	# for i in vector_matrix:
-		# print(i)
-	
-	# html_string = matrix2string(vector_matrix,tagList) # 将矩阵还原成html字符串
-	# 打印html字符串
-	# print(html_string)
-	
-	# 分析矩阵
-	# analyzeMatrix(vector_matrix,tagList)
-	# print(len(vector_matrix))
-	
-	# -------------------下面是统计测试----------------
-	
-	# path = './test_html'
-	
-	# sum_500 = 0
-	# sum_1000 = 0
-	# sum_1500 = 0
-	# sum_2000 = 0
-	
-	# count = 0
-	# sum = 0
-	# for i in os.listdir(path):
-		# html_path = path+'/'+i
-		# if os.path.isfile(html_path):
-			# html_str = readFile(html_path) # 读取本地index.html文件
-			# vector_matrix = generateMatrix(html_str,tagList) # 获取向量矩阵
-			# count+=1
-			# sum+=len(vector_matrix)
-			# if len(vector_matrix) < 500:
-				# sum_500+=1
-			# elif len(vector_matrix) < 1000:
-				# sum_1000+=1
-			# elif len(vector_matrix) < 1500:
-				# sum_1500+=1
-			# elif len(vector_matrix) < 2000:
-				# sum_2000+=1
-	# print(sum_500)
-	# print(sum_1000)
-	# print(sum_1500)
-	# print(sum_2000)
+# 输入一个token_list, 分析输出每个token的重复次数
+def analyzeTokens(token_list):
+	tokens = {}
+	for token in token_list:
+		if token in tokens:
+			tokens[token]+=1
+		else:
+			tokens[token] = 1
+	return tokens
